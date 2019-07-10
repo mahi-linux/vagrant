e7484e0212940763e2d450aaad4c2bb97a0cf9bd
Mahesh <mahi.linux@gmail.com>
2019-07-10 13:46:51 +0530

    Update hosts-update.sh

--- vagrant-yaml/hosts-update.sh
+++ vagrant-yaml/hosts-update.sh
@@ -7,5 +7,5 @@ fi
 # update hosts manually
 sudo echo -e "192.168.10.11   sal-saltp01.example.in  sal-saltp01 salt" >> /etc/hosts
 sudo echo -e "192.168.10.10   gitlab.example.in       gitlab" >> /etc/hosts
-sudo echo -e "192.168.10.12   sal-webp01.example.in   sal-webp01 >> /etc/hosts
-sudo echo -e "192.168.10.13  sal-webp02example.in   sal-webp02 >> /etc/hosts
+sudo echo -e "192.168.10.12   sal-webp01.example.in   sal-webp01" >> /etc/hosts
+sudo echo -e "192.168.10.13  sal-webp02example.in   sal-webp02" >> /etc/hosts
